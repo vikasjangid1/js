d6011554ccd62577ac5382f89b061e722bdfd7e8
Vikas Jangid <121178843+vikasjangid1@users.noreply.github.com>
2023-10-12 11:47:51 +0000

    datatype-summary

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -2,8 +2,34 @@
 
 // 7 types : string , Number, Boolean , null , undefined, symbol , BigInt 
 
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outSideTemp = null; // object
+let userEmail; // undefined
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id > anotherId)
+// const bigNumber = 23534543534552355345n; //bigInt
+
+//console.log(typeof bigNumber)
+
+
 
 
 // Non - Premitive / Reference Type
 
-//Array , Objects
+//Array , Objects, functions 
+
+const heros = ["fkdsj", "fdhs"]
+
+let obj = {
+    name : "vikas",
+    age : 23
+}
+
+function funName(){}
+const fun = function(){
+    console.log("helloworld")
+}
+console.log(typeof fun)
