8f9cc7758fcc82fa507ab9eb07fdae5ddf41d6e9
Vikas Jangid <121178843+vikasjangid1@users.noreply.github.com>
2023-10-18 12:59:55 +0000

    loop

---
+++ 05_Iterations/01_one.js
@@ -0,0 +1,25 @@
+// for 
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5 ){
+//         console.log("5 is the best number")
+//     }
+//     console.log(element)
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Outer loop value: ${i}`)
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value: ${i} and inner loop ${i}`)   
+//         console.log(i + '*' + j + ' = ' + i*j  );
+//     }
+// }
+
+let myArray = ['flash','batman','superman']
+console.log(myArray.length)
+for (let i = 0; i <= myArray.length; i=i+1) {
+    const element = myArray[i];
+    console.log(element)
+}
+
